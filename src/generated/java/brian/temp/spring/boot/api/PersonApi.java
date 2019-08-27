22dae85ab853fb9fca6297d306e4e5752ad1c982
Brian Heo <brian.heo@scotiabank.com>
2019-08-27 11:27:04 -0400

    Update project name

--- src/generated/java/brian/temp/spring/boot/api/PersonApi.java
+++ src/generated/java/brian/temp/spring/boot/api/PersonApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import javax.validation.constraints.*;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-08-26T14:49:09.354-04:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-08-27T11:25:46.769-04:00")
 
 @Api(value = "person", description = "the person API")
 public interface PersonApi {
